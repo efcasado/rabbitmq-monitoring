196f6dac585f164c0ce9d8bd0e835fb5557a8d01
Enrique Fernandez <efcasado@gmail.com>
2016-10-08 08:49:24 +0200

    Add Grafana and a second sender

--- volumes/python/code/send.py
+++ volumes/python/code/send.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python
+import os
 import pika
 import random
 import time
+
+
+queue = os.environ['RABBITMQ_QUEUE']
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='rabbitmq'))
 channel = connection.channel()
 
+
 while True:
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue=queue)
 
     channel.basic_publish(exchange='',
-                      routing_key='hello',
+                      routing_key=queue,
                       body='Hello World!')
 
     print(" [x] Sent 'Hello World!'")
 
     time.sleep(random.random() * random.randint(1, 2))
+
+
 connection.close()
